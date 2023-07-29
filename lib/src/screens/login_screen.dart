b76afc723cc37b69d3ba9c06a3b43f0ffdff321e
rubayet211 <rubayet211@gmail.com>
2023-07-29 14:27:33 +0600

    3rd commit

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Form(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
